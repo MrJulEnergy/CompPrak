139f2549e4b8a239351b12af4e92bf4ec8e13ba1
Julian Peters <uni@julian-peters.com>
2024-01-10 22:24:30 +0100

    bin am verzweifeln

--- visualize.py
+++ visualize.py
@@ -7,7 +7,7 @@ class Visualize:
         self.trajectory = trajectory  #trajectory[i, j, k]; i=particle, j=x/y, k=timestep
         self.fps = fps
         self.bounds = bounds
-        self.window_size = (500, 300) 
+        self.window_size = (800, 300) 
 
         self.window_title = "Population Simulation"
      
@@ -19,46 +19,30 @@ class Visualize:
         self.n_particles = self.trajectory.shape[0]
         self.n_timesteps = self.trajectory.shape[2]
 
-        self.bounds_center = [self.bounds[1]+self.bounds[0]/2, self.bounds[3]+self.bounds[2]/2]
+        self.bounds_center = np.array([(self.bounds[1]+self.bounds[0])/2, (self.bounds[3]+self.bounds[2])/2])
         self.bounds_width = self.bounds[1]-self.bounds[0]
         self.bounds_height = self.bounds[3]-self.bounds[2]
         self.bounds_aspect = self.bounds_width / self.bounds_height
-        self.relevant_window_size_index = np.argmin(self.window_size)
-        print(self.relevant_window_size_index)
-       
+        self.window_aspect = self.window_size[0] / self.window_size[1]
+        self.min_idx = np.argmin(self.window_size)
+        
         self.setup_window()
         self.run()
 
         
-    def transform_coords(self, point: np.array):
-        # relativ zur box mitte
-        rel_point = point-self.bounds_center
-        # jetzt von 0 bis bounds_width/height 
-        new_point = rel_point + np.array([self.bounds_width, self.bounds_height])/2
-        # an auflösung anpassen:
-        new_new_point = new_point * np.array([self.window_size[self.relevant_window_size_index]/self.bounds_width, self.window_size[self.relevant_window_size_index]/self.bounds_height])
-        # in die mitte schieben:
-        new_new_new_point = new_new_point + np.array([self.window_size[self.relevant_window_size_index-1]/2, self.window_size[self.relevant_window_size_index]/2])
-        return new_new_new_point
+
     
+    def transform_coords(self, point: np.array):
+        rel_point = (point - self.bounds_center) * np.array([1, -1]) + np.array([self.bounds_width/2, self.bounds_height/2])
+        
+        
+        if self.min_idx == 0:
+            rel_point *= np.array([self.window_size[0]/self.bounds_width, self.window_size[0]/self.bounds_height*self.window_aspect])
+        else:
+            rel_point *= np.array([self.window_size[1]/self.bounds_width*self.window_aspect, self.window_size[1]/self.bounds_height])
+            print(rel_point)
 
-    def draw_grid(self):
-        grid_color = (100, 100, 100)
-        grid_size = 1.0
-
-        # Calculate grid bounds in world coordinates
-        grid_x_min, grid_x_max = math.floor(self.bounds[0] / grid_size) * grid_size, math.ceil(self.bounds[1] / grid_size) * grid_size
-        grid_y_min, grid_y_max = math.floor(self.bounds[2] / grid_size) * grid_size, math.ceil(self.bounds[3] / grid_size) * grid_size
-
-        # Draw vertical lines
-        for x in np.arange(grid_x_min, grid_x_max + grid_size, grid_size):
-            grid_pos = self.transform_coords(np.array([x, self.bounds[2]]))
-            pygame.draw.line(self.screen, grid_color, (grid_pos[0], 0), (grid_pos[0], self.window_size[1]))
-
-        # Draw horizontal lines
-        for y in np.arange(grid_y_min, grid_y_max + grid_size, grid_size):
-            grid_pos = self.transform_coords(np.array([self.bounds[0], y]))
-            pygame.draw.line(self.screen, grid_color, (0, grid_pos[1]), (self.window_size[0], grid_pos[1]))
+        return rel_point
 
     def setup_window(self):
         self.running = True
@@ -77,7 +61,6 @@ class Visualize:
             self.check_events()
             self.screen.fill(self.background_color)
             self.draw_particles(i)
-            self.draw_grid()
             i+=1
             pygame.display.update()
             dt = self.clock.tick(self.fps) / 1000
@@ -99,9 +82,13 @@ class Visualize:
 
 
 if __name__ == "__main__":
-    x = np.linspace(0, 2*np.pi, 550)
-    y = np.sin(x)
-    traj = np.array([[x, y]])
-    bounds = np.array([0, 2*np.pi, -1, 1])
+    #x = np.linspace(0, 2*np.pi, 550)
+    #y = np.sin(x)
+    #traj = np.array([[x, y]])
+    #bounds = np.array([0, 2*np.pi, -1, 1])
 
+    x = np.linspace(0, 1, 250)
+    y = x
+    traj = np.array([[x, y]])
+    bounds = np.array([0, 1, 0, 1])
     vis = Visualize(traj, fps=100, bounds=bounds)
